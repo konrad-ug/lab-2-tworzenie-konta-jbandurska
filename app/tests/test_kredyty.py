2f69acb3a3d0c8c4e64cc64a627c15086df787e4
jbandurska <julka.ban@gmail.com>
2022-11-10 19:31:21 +0100

    refactor

--- app/tests/test_kredyty.py
+++ app/tests/test_kredyty.py
@@ -1,4 +1,5 @@
-import unittest, parameterized
+import unittest
+from parameterized import parameterized
 
 from ..Konto import Konto
 
@@ -6,49 +7,17 @@ class TestKredyty(unittest.TestCase):
 
     def setUp(self):
         self.konto = Konto('darek', 'kowalski', '02271901334')
-        self.kwota = 500
 
     @parameterized.expand([
         ([-100,100,100,100], 500, True, 500),
-        ([-200,100,200,100,30], 600, True, 600)
+        ([-100,100,100,-100], 500, False, 0),
+        ([-100,100,200,-100,500], 500, True, 500),
+        ([-100,100,-100,100,-500], 500, False, 0),
+        ([-100], 500, False, 0)
     ])
-    def test_udzielony_kredyt_ostatnie_3_transakcje(self, historia, kwota, oczekiwanyWynik, saldo):
+    def test_kredyt(self, historia, kwota, oczekiwany_wynik, oczekiwane_saldo):
         self.konto.historia = historia
         czyUdzielony = self.konto.zaciagnij_kredyt(kwota)
-        self.assertEqual(czyUdzielony, oczekiwanyWynik)
-        self.assertEqual(self.konto.saldo, saldo)
-
-    def test_nieudzielony_kredyt_ostatnie_3_transakcje(self):
-        self.konto.historia = [-100,100,-100,100]
-
-        czyUdzielony = self.konto.zaciagnij_kredyt(self.kwota)
-
-        self.assertTrue(not(czyUdzielony))
-        self.assertEqual(self.konto.saldo, 0)
-
-    def test_udzielony_kredyt_ostatnie_5_transakcji(self):
-        self.konto.historia = [-100,100,300,-100,200,200]
-
-        czyUdzielony = self.konto.zaciagnij_kredyt(self.kwota)
-
-        self.assertTrue(czyUdzielony)
-        self.assertEqual(self.konto.saldo, self.kwota)
-
-    def test_nieudzielony_kredyt_ostatnie_5_transakcji(self):
-        self.konto.historia = [-100,100,200,100,-20]
-
-        czyUdzielony = self.konto.zaciagnij_kredyt(self.kwota)
-
-        self.assertTrue(not(czyUdzielony))
-        self.assertEqual(self.konto.saldo, 0)
-
-    def test_nieudzielony_kredyt_za_malo_transakcji(self):
-        self.konto.historia = [-100]
-
-        czyUdzielony = self.konto.zaciagnij_kredyt(self.kwota)
-
-        self.assertTrue(not(czyUdzielony))
-        self.assertEqual(self.konto.saldo, 0)
-
-
+        self.assertEqual(czyUdzielony, oczekiwany_wynik)
+        self.assertEqual(self.konto.saldo, oczekiwane_saldo)
         
